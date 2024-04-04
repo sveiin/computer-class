921b4991fd72bf4295c16534844f541c8e4b0b3c
sveiin <veiin9230@gmail.com>
2024-04-04 09:42:43 +0900

    interface members

---
+++ lab07_inheritance/src/com/itwill/interface02/InterfaceMain02.java
@@ -0,0 +1,16 @@
+package com.itwill.interface02;
+
+public class InterfaceMain02 {
+
+	public static void main(String[] args) {
+		// Test 인터페이스의 public으로 공개된 멤버 사용
+		System.out.println(Test.VERSION);
+		Test.staticMethod();
+		
+		// Test 인터페이스의 구현 객체 생성 & 메서드 호출
+		Test t = new TestImpl(); // 다형성: SuperType var = new SubType();
+		t.defaultMethod();
+		t.test();
+	}
+
+}
