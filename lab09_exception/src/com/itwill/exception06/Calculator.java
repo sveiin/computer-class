55c090e6808506edd7b224c7d46a4c99feac6bd0
sveiin <veiin9230@gmail.com>
2024-04-05 13:19:19 +0900

    throw, throws

---
+++ lab09_exception/src/com/itwill/exception06/Calculator.java
@@ -0,0 +1,13 @@
+package com.itwill.exception06;
+
+public class Calculator {
+
+	public int divide(int x, int y) throws Exception {
+		if(y != 0) {
+			return x / y;
+		}
+		
+		throw new Exception("y는 0이 될 수 없음");
+	}
+			
+}
