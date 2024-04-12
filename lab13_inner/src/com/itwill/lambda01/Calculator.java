7114ef675ec512c70748ef015afa33df599d8cfb
sveiin <veiin9230@gmail.com>
2024-04-12 19:03:14 +0900

    inner interface

---
+++ lab13_inner/src/com/itwill/lambda01/Calculator.java
@@ -0,0 +1,23 @@
+package com.itwill.lambda01;
+
+public class Calculator {
+    
+    // 함수형 인터페이스: 추상 메서드를 오직 1개만 갖는 인터페이스
+    @FunctionalInterface
+    public interface Calculable {
+        double calculate(double x, double y);
+    }
+    
+    private double x;
+    private double y;
+    
+    public Calculator(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double calculate(Calculable calc) {
+        return calc.calculate(x, y);
+    }
+
+}
